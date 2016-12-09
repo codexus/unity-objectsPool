b95320f6251731ef1a6eeb99b473a5c00fc2e0a4
codexus <chrustol@gmail.com>
2016-12-09 14:48:58 +0100

    Update Pool.cs
    Changed C# version of stack implementation to custom basic array stack.

--- Pool.cs
+++ Pool.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
-using System;
 
-public class Pool {
-
+/// <summary>
+/// Fast implementation of Pool object using array as a stack
+/// </summary>
+public class Pool
+{
     #region variables
 
-    private int m_initialSize = 2; // initial size of the pool
+    private int size = 2; // initial size of the pool
     private GameObject m_poolObject; // Prefab that will be instatitated
     private Transform m_objectsHolder; // Prefab that will be instatitated
 
-    private Stack<GameObject> m_pool = new Stack<GameObject>(); // pool stack
-    private int m_activeObjects; // number of objects currently on scene
+    //private Stack<GameObject> m_pool = new Stack<GameObject>(); // pool stack
+    private GameObject[] m_poolStack;
+
+    private int m_activeCount; // number of objects currently on scene
+    private int m_inactiveCount; // number of objects in the pool ready for spawning
 
     #endregion
 
@@ -23,7 +27,7 @@ public class Pool {
     /// </summary>
     public int ActiveCount
     {
-        get { return m_activeObjects; }
+        get { return m_activeCount; }
     }
 
     /// <summary>
@@ -31,37 +35,62 @@ public class Pool {
     /// </summary>
     public int InactiveCount
     {
-        get { return m_pool.Count; }
+        get { return m_inactiveCount; }
     }
 
     #endregion
 
     #region methods
 
-    public Pool(GameObject gameObject, Transform objectsHolder = null)
+    /// <summary>
+    /// Creates a new Pool for holding game objects.
+    /// </summary>
+    /// <param name="gameObject">GameObject prefab to spawn</param>
+    /// <param name="parentTransform">Transform parent for spawned objects. If the value is left as null, 
+    /// the spawned objects will be attached to scene root.</param>
+    public Pool(GameObject gameObject, Transform parentTransform = null)
     {
-        m_pool = new Stack<GameObject>();
-        m_objectsHolder = objectsHolder;
+        m_poolStack = new GameObject[size];
+        m_objectsHolder = parentTransform;
         m_poolObject = gameObject;
     }
-    
+
+    /// <summary>
+    /// Spawns the object. Note that the Awake() and Start() will be called only once. OnDisable() and OnEnable() will be called insted,
+    /// since the method calls SetActive(true) on spawned Object;
+    /// </summary>
+    /// <returns>Instance from prefab</returns>
     public GameObject Spawn()
     {
         return Spawn(Vector3.zero);
     }
-    
+
+    /// <summary>
+    /// Spawns the object with desired position. Note that the Awake() and Start() will be called only once. OnDisable() and OnEnable() will be called insted,
+    /// since the method calls SetActive(true) on spawned Object;
+    /// </summary>
+    /// <param name="position">Position where the object shoul be spawned</param>
+    /// <returns>Instance from prefab</returns>
+
     public GameObject Spawn(Vector3 position)
     {
         return Spawn(position, Quaternion.identity);
     }
 
+    /// <summary>
+    /// Spawns the object with desired position and rotation. Note that the Awake() and Start() will be called only once. OnDisable() and OnEnable() will be called insted,
+    /// since the method calls SetActive(true) on spawned Object;
+    /// </summary>
+    /// <param name="position">Position where the object shoul be spawned</param>
+    /// <param name="rotation">Rotation of the spawned object</param>
+    /// <returns></returns>
     public GameObject Spawn(Vector3 position, Quaternion rotation)
     {
         GameObject go; // object to return
 
-        if (m_pool.Count == 0)
+        if (m_inactiveCount == 0)
         {
-            if(m_objectsHolder == null)
+            if (m_objectsHolder == null)
             {
                 go = GameObject.Instantiate(m_poolObject, position, rotation) as GameObject;
             }
@@ -72,23 +101,53 @@ public class Pool {
         }
         else
         {
-            go = m_pool.Pop();
+            go = m_poolStack[--m_inactiveCount];
         }
 
-        go.transform.position = position;
+        // set position and rotation
+        go.transform.position = position; 
         go.transform.rotation = rotation;
         go.SetActive(true);
 
-        m_activeObjects++;
+        // increment active count
+        m_activeCount++;
         return go;
     }
 
+    /// <summary>
+    /// Returns the GameObject back to pool 
+    /// </summary>
+    /// <param name="go"></param>
     public void Remove(GameObject go)
     {
+        if (m_inactiveCount == m_poolStack.Length)
+        {
+            // Double the pools size if it's full.
+            EnlargeStack(size * 2);
+        }
+
         go.SetActive(false);
-        m_pool.Push(go);
-        m_activeObjects--;
+
+        m_activeCount--; // decrement active count
+        m_poolStack[m_inactiveCount++] = go;
     }
-    
+
+    /// <summary>
+    /// Enlarges the pool stack by provided size
+    /// </summary>
+    /// <param name="newSize"></param>
+    private void EnlargeStack(int newSize)
+    {
+        if (newSize <= size) return; // if new size is same or less size
+
+        GameObject[] tmp = new GameObject[newSize];
+        for (int i = 0; i < size; i++)
+        {
+            tmp[i] = m_poolStack[i];
+        }
+        m_poolStack = tmp;
+        size = newSize;
+    }
+
     #endregion
 }
